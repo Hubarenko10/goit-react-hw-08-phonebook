9d9d7d8cf3093427a4e151f284d70a7efe702ff1
Hubarenko10 <103453171+Hubarenko10@users.noreply.github.com>
2023-01-29 19:14:27 +0000

    Deploying to gh-pages from @ Hubarenko10/goit-react-hw-08-phonebook@b72ffb00410248a5430dfde1a7ababb637d30ca4 🚀

--- static/js/75.62717c70.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[75],{6075:function(e,r,n){n.r(r),n.d(r,{default:function(){return t}});var a=n(184);function t(){return(0,a.jsx)("div",{children:(0,a.jsxs)("h1",{children:["Task manager welcome page"," ",(0,a.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
-//# sourceMappingURL=75.62717c70.chunk.js.map
